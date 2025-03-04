7a960d4f7342cc1cbd9ab490adc392b00248f521
zenakiyo <zenakiyo@gmail.com>
2025-03-05 00:45:41 +0530

    fronted-mac

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,31 @@
-import { Button } from "./components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { useEffect, useState } from "react";
 
 function App() {
+  const [totalSpent, setTotalSpent] = useState(0);
+  useEffect(() => {
+    async function fetchTotal() {
+      const res = await fetch("/api/expenses/total-spent");
+      const data = await res.json();
+      setTotalSpent(data.total);
+    }
+    fetchTotal();
+  });
   return (
-    <>
-      <Button>Click Me</Button>
-    </>
+    <Card className="w-[350px] mx-auto">
+      <CardHeader>
+        <CardTitle>Total Spent</CardTitle>
+        <CardDescription>The total amount you've spent</CardDescription>
+      </CardHeader>
+      <CardContent>{totalSpent}</CardContent>
+    </Card>
   );
 }
 
